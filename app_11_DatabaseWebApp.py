f901af1feea9b64c3093ae4f544276dbbd860788
wrobellkatarzyna <63967309+wrobellkatarzyna@users.noreply.github.com>
2020-08-19 10:52:53 +0200

    db imported

---
+++ app_11_DatabaseWebApp.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+# console from app_11_DatabaseWebApp import db
+
+app = Flask(__name__)
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/height_collector'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        print(email, height)
+        return render_template("success.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
